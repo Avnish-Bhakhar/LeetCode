fd1f82bc376fe5079be877b44ad9c2c3873251fa
Avnish Bhakhar <103834051+Avnish-Bhakhar@users.noreply.github.com>
2026-06-24 10:55:26 +0530

    Time: 2 ms (62.38%), Space: 10.8 MB (80.25%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>freq;
+
+        for (auto i : s)
+        {
+            freq[i]++;
+        }
+
+        vector<pair<int,char>>v;
+        for (auto it : freq){
+            v.push_back({it.second , it.first});
+        }
+
+        sort(v.begin(),v.end());
+        reverse(v.begin(),v.end());
+
+        string ans = "";
+        
+        for (auto it : v)
+        {
+            while(it.first--)
+            {
+                ans+=it.second;
+            }
+        }
+
+        return ans;
+
+    }
+};
