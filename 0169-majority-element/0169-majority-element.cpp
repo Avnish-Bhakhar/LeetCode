4a8ac21f598f785c4f2336e89280f2f288a1c394
Avnish Bhakhar <103834051+Avnish-Bhakhar@users.noreply.github.com>
2026-06-29 10:42:39 +0530

    Time: 0 ms (100%), Space: 28.3 MB (8.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int>m;
+        // map<int, int>m;
 
-        for (int i = 0; i<nums.size();i++)
+        // for (int i = 0; i<nums.size();i++)
+        // {
+        //     m[nums[i]]++;
+        // }
+        // int ele = INT_MIN;
+        // int ans = 0;
+        // for (auto i: m)
+        // {
+        //     if (i.second>ele)
+        //     {
+        //         ele = i.second;
+        //         ans = i.first;
+        //     }
+        // }
+        // return ans;
+
+        int n = nums.size();
+        int cnt = 0;
+        int maj = NULL;
+
+        for (int i = 0; i<n;i++)
         {
-            m[nums[i]]++;
-        }
-        int ele = INT_MIN;
-        int ans = 0;
-        for (auto i: m)
-        {
-            if (i.second>ele)
+            if(cnt == 0)
             {
-                ele = i.second;
-                ans = i.first;
+                maj = nums[i];
+                cnt = 1;
+            }
+            else if(nums[i]== maj)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
             }
         }
-        return ans;
+        return maj;
+
     }
 };
