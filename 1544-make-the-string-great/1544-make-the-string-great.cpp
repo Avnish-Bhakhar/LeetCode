4a923ad0aab7cc3b57628c1a6e7cb007a270bb7b
Avnish Bhakhar <103834051+Avnish-Bhakhar@users.noreply.github.com>
2026-06-30 09:58:16 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.13%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans = "";
+
+        for(char ch: s)
+        {
+            if(!ans.empty() && abs(ch - ans.back() ) == 32 )
+            {
+                ans.pop_back();
+            }else
+            {
+                ans.push_back(ch);
+            }
+
+        }
+        return ans;
+    }
+};
