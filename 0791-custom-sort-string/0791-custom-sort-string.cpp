0b024fb91c944d0fc36a75befc5745e1bd59db25
Avnish Bhakhar <103834051+Avnish-Bhakhar@users.noreply.github.com>
2026-07-02 10:49:39 +0530

    Time: 0 ms (100%), Space: 8.1 MB (86.98%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+
+        vector<int> freq(26, 0);
+
+        // Count frequency of characters in s
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        string ans = "";
+
+        // Add characters according to custom order
+        for (char ch : order) {
+            while (freq[ch - 'a'] > 0) {
+                ans += ch;          // or ans.push_back(ch);
+                freq[ch - 'a']--;
+            }
+        }
+
+        // Add remaining characters
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] > 0) {
+                ans += (char)(i + 'a');
+                freq[i]--;
+            }
+        }
+
+        return ans;
+    }
+};
