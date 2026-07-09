eec66f963a8540cd68dfcb60050083f6895d64ae
Avnish Bhakhar <103834051+Avnish-Bhakhar@users.noreply.github.com>
2026-07-09 09:37:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (68.34%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string st;
+
+        for(char ch: s)
+        {
+            if(isdigit(ch))
+            {
+                st.pop_back();
+            }
+            else
+            {
+                st.push_back(ch);
+            }
+        }
+        return st;
+    }
+};
